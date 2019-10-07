188f01b9ca8eb2789a0e15f72f9e2b08d8a66e0e
Elina Andersson <elina.andersson3@learnet.se>
2019-10-07 14:57:26 +0200

    Added DataAccess

---
+++ DataAccess/SchoolContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class SchoolContext : DbContext
+    {
+        private const string connectinString = "server=LAPTOP-UIJVU0KV;Database=EFCore;Trusted_Connection=True;";
+
+
+        //local host = det som kommer fam när vi connectras vår sql server vanligtvis kommer vi inte bara kunna skriva connection
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectinString);
+        }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public  DbSet<Teacher> Teachers{ get; set; }
+        public DbSet<Grade> Grades { get; set; }
+    }
+}
