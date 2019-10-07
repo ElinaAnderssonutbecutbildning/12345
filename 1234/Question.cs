aef82b97d4bcaf701e6343207d49cff270b9088a
Elina Andersson <elina.andersson3@learnet.se>
2019-10-07 10:41:55 +0200

    Question

---
+++ 1234/Question.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace _1234
+{
+    public class Question
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int QuestionID { get; set; }
+        public string Questionname { get; set; }
+        public ICollection<Tests> Testss { get; set; }
+    }
+}
+
