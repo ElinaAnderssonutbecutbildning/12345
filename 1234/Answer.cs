755ab3e70a50d743a67b891988f37a581ee19ae4
Noah Johansson <Noahjohan@hotmail.com>
2019-10-07 12:50:37 +0200

    Added connection between Answer.cs and AnswelAlt.cs

--- 1234/Answer.cs
+++ 1234/Answer.cs
@@ -7,7 +7,9 @@ namespace ConsoleApp1
     public class Answer
     {
         public int AnswerID { get; set; }
+        public int AnswerAltID { get; set; }
 
+        public AnswerAlt AnswerAlt { get; set; }
         public ICollection<TestAnswer> TestAnswers { get; set; }
     }
 }
