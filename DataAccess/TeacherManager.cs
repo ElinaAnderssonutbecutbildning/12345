e2493b0f6afe6bbe164ed8018c51234249e04c31
Elina Andersson <elina.andersson3@learnet.se>
2019-10-08 11:09:56 +0200

    Teacher Manager

--- DataAccess/TeacherManager.cs
+++ DataAccess/TeacherManager.cs
@@ -1,9 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ConsoleApp1;
 using DataInterface;
 
 namespace DataAccess
 {
+    public class TeacherManager
+    {
+        public void AddTeacher(string name)
+        {
+            using(var schoolContext = new SchoolContext())
+            {
+                var teacher1 = new Teacher();
+                teacher1.Name = "Göran";
+                schoolContext.Teachers.Add(teacher1);
+
+                var teacher2 = new Teacher();
+                teacher2.Name = "Maria";
+                schoolContext.Teachers.Add(teacher2);
+            }
+        }
+    }
 
 }
