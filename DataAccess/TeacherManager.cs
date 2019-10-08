eb2868de16a9d57940809d1c546dff98dbb04dfd
Kim Lennings <lennings@gmail.com>
2019-10-08 11:17:46 +0200

    fixed interface add to teachermanager

--- DataAccess/TeacherManager.cs
+++ DataAccess/TeacherManager.cs
@@ -6,7 +6,7 @@ using DataInterface;
 
 namespace DataAccess
 {
-    public class TeacherManager
+    public class TeacherManager : ITeacher
     {
         public void AddTeacher(string name)
         {
