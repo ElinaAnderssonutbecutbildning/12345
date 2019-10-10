66aeb6f37549c1de091a3dabff6a82af179583ba
cinoZ <till.peterswenson@gmail.com>
2019-10-10 14:09:22 +0200

    AnswerAlt maualy reverted

--- DataInterface/AnswersAlt.cs
+++ DataInterface/AnswersAlt.cs
@@ -1,16 +1,21 @@
-﻿using DataInterface;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ConsoleApp1
 {
     public class AnswerAlt
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int AnswerAltID { get; set; }
         public int QuestionAnswerID { get; set; }
-        public string AlterText { get; set; }
-        
-        public QuestionAnswer QuestionAnswer { get; set; }
+
+        public string[] AnswerAlternatives { get; set; }
+        public int CorrectAlternativeIndex { get; set; }
+
         public ICollection<Question> Questions { get; set; }
-        //public ICollection<Answer> Answers { get; set; }
+
+        public QuestionAnswer QuestionAnswer { get; set; }
     }
 }
